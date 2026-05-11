661f66564b1bfa8aaf0e25e499841f12d1e1970f
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-05-11 11:49:33 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projectssssss 🚀");
+    res.send("Hello from Node.js DevOps Projectssssssssss 🚀");
 });
 
 app.get('/health', (req, res) => {
